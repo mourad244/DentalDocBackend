9498bf6dc23a062ff118aaf2148e96f0dd71db6f
mourad244 <mouradvilla2@gmail.com>
2024-01-31 16:44:49 +0100

    add

--- startup/cors.js
+++ startup/cors.js
@@ -1,7 +1,5 @@
 const cors = require("cors");
 
 module.exports = function (app) {
-  app.use(
-    cors({ origin: "https://dentaldocfrontend-cb29443ba68e.herokuapp.com" })
-  );
+  app.use(cors());
 };
