8be570dbf096c420d1856311ad07410bdb9ce51e
mourad244 <mouradvilla2@gmail.com>
2024-10-06 17:07:03 +0100

    add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
-
+//..
 const port = process.env.PORT || 3900;
 const server = app.listen(port, () =>
   logger.info(`Listening on port ${port}...`)
