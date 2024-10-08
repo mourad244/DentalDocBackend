5522025e28ebea5b0fef104e1753eddf8cf9df18
mourad244 <mouradvilla2@gmail.com>
2024-10-08 20:53:42 +0100

    add

--- routes/auth.js
+++ routes/auth.js
@@ -9,12 +9,12 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   const { error } = validations.user(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("email ou mot de pass invalid.");
+  let user = await User.findOne({ nom: req.body.nom });
+  if (!user) return res.status(400).send("nom ou mot de pass invalid.");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword)
-    return res.status(400).send("email ou mot de pass invalide.");
+    return res.status(400).send("nom ou mot de pass invalide.");
 
   const token = user.generateAuthToken();
   res.send(token);
