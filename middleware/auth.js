e9cf87826316f44a313e6a15a0e09b742fdf802c
mourad244 <mouradvilla2@gmail.com>
2024-10-28 20:16:33 +0100

    add

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   const requireAuth = process.env.REQUIRE_AUTH;
   const jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
   if (!requireAuth) return next();
-
+  console.log(req.user);
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
   try {
